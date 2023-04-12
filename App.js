f2d8a04dac9c8ac74967c58fc3c9e695df6bf35b
Jose Machado <jose.mqmachado@gmail.com>
2023-04-12 17:35:32 -0300

    primeiro

--- App.js
+++ App.js
@@ -1,20 +1,171 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { StyleSheet, Text, View, Image, TextInput, ScrollView, FlatList, Button, TouchableOpacity, Alert } from "react-native";
+import { Octicons } from "@expo/vector-icons";
 
 export default function App() {
+
+  const [raca, setRaca] = useState('')
+  const [on, setOn] = useState(false)
+  const [image, setImage] = useState([
+    { foto: require('./assets/alemao.jpg'), nome: 'Bob', anos: '3 anos', raca: 'Alemão', sexo: 'M', key: 1 },
+    { foto: require('./assets/dalm.jpg'), nome: 'Rex', anos: '6 anos', raca: 'Dalmata', sexo: 'M', key: 2 },
+    { foto: require('./assets/poddle.jpg'), nome: 'Maggie', anos: '4 anos', raca: 'Poodle', sexo: 'F', key: 3 },
+    { foto: require('./assets/image2.jpg'), nome: 'Mel', anos: '2 anos', raca: 'Beagle', sexo: 'F', key: 4 },
+  ])
+
+  function Verificar() {
+    setOn(!on)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <View style={styles.barra}>
+        <Image style={styles.logo} source={require("./assets/cape.jpg")} />
+        <Text style={styles.item}>Match Pets</Text>
+        <Octicons name="gear" size={30} color="black" />
+      </View>
+
+      <ScrollView style={styles.principal}>
+
+
+        <View style={styles.barrapesquisa}>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite a raça..."
+            placeholderTextColor={"gray"}
+            onChangeText={(racatal) => { setRaca(racatal), setOn(false) }}
+          />
+          <View style={styles.buttonContainer}>
+            <Button title="BUSCAR" color='#ab0286' onPress={Verificar} />
+          </View>
+        </View>
+
+        <FlatList
+          numColumns={1}
+          keyExtractor={(item) => item.key}
+          data={image}
+          renderItem={({ item }) => (
+            on ? [
+
+              item.raca == raca ?
+                < View style={styles.dogs} >
+                  <View style={styles.dogs2}>
+                    <Text>{item.nome}</Text>
+                    <Image style={styles.dog} source={item.foto} />
+                    <Text>{item.anos}</Text>
+                    <Text>{item.sexo}</Text>
+                    <Text>{item.raca}</Text>
+                  </View>
+
+                  <TouchableOpacity
+                    onPress={() => Alert.alert('DEU LIKE!')}
+                  >
+                    <Image style={styles.image} source={require("./assets/like.png")} />
+
+                  </TouchableOpacity>
+
+
+                  <TouchableOpacity
+                    onPress={() => Alert.alert('DEU DISLIKE!')}
+                  >
+                    <Image style={styles.image} source={require("./assets/dislike.jpg")} />
+
+                  </TouchableOpacity>
+
+                </View>
+
+                :
+
+                <Text></Text>
+            ]
+              :
+
+              <Text></Text>
+          )}
+        />
+
+      </ScrollView >
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  image: {
+    width: 50,
+    height: 50,
+  },
+
+  item: {
+    paddingTop: 5,
+    fontSize: 20,
+    color: "#ff3399",
+    fontWeight: "bold",
+  },
+
+  barra: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-start",
+    paddingTop: 50,
+    borderWidth: 1,
+    borderColor: "#ff3399",
+  },
+  logo: {
+    width: 40,
+    height: 40,
+  },
+
+  dog: {
+    width: 140,
+    height: 200,
+    borderWidth: 4,
+    borderColor: "#ff3385",
+    borderRadius: 10,
+  },
+
+  input: {
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 10,
+    width: 290,
+    height: 33,
+  },
+
+  barrapesquisa: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  principal: {
+    backgroundColor: "pink",
+  },
+
+  dogs: {
+    flex: 1,
+    flexDirection: "row",
+    borderWidth: 1,
+    borderColor: "#777",
+    padding: 8,
+    margin: 15,
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+
+  dogs2: {
+    flexDirection: "column",
+    borderColor: "#777",
+    width: 150,
+    alignItems: "center",
+  },
+
+  buttonContainer: {
+    backgroundColor: 'blue'
+  },
+
 });
---
+++ readme.md
@@ -0,0 +1,40 @@
+Match Pets
+Match Pets é um aplicativo de encontro para cachorros baseado em Tinder, permitindo que os usuários possam encontrar novos amigos caninos ou até mesmo marcar encontros para cruzamentos.
+
+Funcionalidades
+Os usuários podem pesquisar cachorros por raça.
+Os usuários podem dar like ou dislike em cachorros.
+Os usuários podem visualizar os cachorros que deram like.
+Os usuários podem visualizar os cachorros que deram dislike.
+Os usuários podem visualizar os dados de um cachorro, como nome, idade, sexo e raça.
+Tecnologias Utilizadas
+O aplicativo foi desenvolvido utilizando as seguintes tecnologias:
+
+React Native
+Expo
+JavaScript
+CSS
+
+Como Executar
+Para executar o Match Pets, siga as instruções abaixo:
+
+Clone o repositório:
+bash
+Copy code
+git clone https://github.com/seu_usuario/match-pets.git
+Entre na pasta do projeto:
+bash
+Copy code
+cd match-pets
+Instale as dependências:
+Copy code
+npm install
+Execute o projeto:
+sql
+Copy code
+npm start
+Escaneie o QR Code com o aplicativo Expo em seu smartphone ou utilize um emulador para visualizar a aplicação.
+Contribuições
+Autores
+José Antônio de Moura Queiroz Machado
+Irinaldo Adelino Ramos Junior
